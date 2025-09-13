3082cfd8bbddd448c7476721627b4007ff91f2c0
Mahdi <mahdighadiriafzal@gmail.com>
2025-09-13 16:05:59 +0330

    Update crud.py

--- src/crud.py
+++ src/crud.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
-from . import models
+
+from . import models, schemas
 
 
 def create_expense(db: Session, description: str, amount: float):
@@ -16,3 +17,25 @@ def get_expenses(db: Session, skip: int = 0, limit: int = 100):
 
 def get_expense(db: Session, expense_id: int):
     return db.query(models.Expense).filter(models.Expense.id == expense_id).first()
+
+
+def update_expense(db: Session, expense_id: int, expense: schemas.ExpenseUpdate):
+    db_expense = (
+        db.query(models.Expense).filter(models.Expense.id == expense_id).first()
+    )
+    if expense.description:
+        db_expense.description = expense.description
+    if expense.amount:
+        db_expense.amount = expense.amount
+    db.commit()
+    db.refresh(db_expense)
+    return db_expense
+
+
+def delete_expense(db: Session, expense_id: int):
+    db_expense = (
+        db.query(models.Expense).filter(models.Expense.id == expense_id).first()
+    )
+    if db_expense:
+        db.delete(db_expense)
+        db.commit()

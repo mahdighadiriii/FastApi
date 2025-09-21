a07ae4c391fbe0b7723d468219303c83b2f8472f
Mahdi <mahdighadiriafzal@gmail.com>
2025-09-21 22:00:12 +0330

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -24,3 +24,30 @@ class ExpenseOut(ExpenseBase):
 class ExpenseUpdate(BaseModel):
     description: Optional[str] = None
     amount: Optional[float] = None
+
+
+class UserBase(BaseModel):
+    username: str
+
+
+class UserCreate(BaseModel):
+    username: str
+    password: str
+
+
+class UserOut(BaseModel):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+
+class Token(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str
+
+
+class LoginForm(BaseModel):
+    username: str
+    password: str

d43aeab4445811ce26967e803c2b5ea9d5f54273
Mahdi <mahdighadiriafzal@gmail.com>
2025-09-21 19:21:19 +0330

    Create expenses.py

---
+++ app/crud/expenses.py
@@ -0,0 +1,39 @@
+from sqlalchemy.orm import Session
+
+from ..models import Expense
+from ..schemas import ExpenseCreate, ExpenseUpdate
+
+
+def create_expense(db: Session, expense: ExpenseCreate):
+    db_expense = Expense(**expense.model_dump())
+    db.add(db_expense)
+    db.commit()
+    db.refresh(db_expense)
+    return db_expense
+
+
+def get_expenses(db: Session):
+    return db.query(Expense)
+
+
+def get_expense(db: Session, expense_id: int):
+    return db.query(Expense).filter(Expense.id == expense_id).first()
+
+
+def update_expense(db: Session, expense_id: int, expense: ExpenseUpdate):
+    db_expense = get_expense(db, expense_id)
+    if not db_expense:
+        return None
+    update_data = expense.dict(exclude_unset=True)
+    for key, value in update_data.items():
+        setattr(db_expense, key, value)
+    db.commit()
+    db.refresh(db_expense)
+    return db_expense
+
+
+def delete_expense(db: Session, expense_id: int):
+    db_expense = get_expense(db, expense_id)
+    if db_expense:
+        db.delete(db_expense)
+        db.commit()

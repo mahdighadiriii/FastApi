c7bea0069f0f5428091ae9dedee4f3182d6a7d22
Mahdi <mahdighadiriafzal@gmail.com>
2025-09-21 21:59:51 +0330

    Update database.py

--- app/database.py
+++ app/database.py
@@ -6,7 +6,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
-SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "SQLALCHEMY_DATABASE_URL",
+)
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}

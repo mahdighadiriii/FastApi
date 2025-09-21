68c78cfb03e574359163147405957d53daea354e
Mahdi <mahdighadiriafzal@gmail.com>
2025-09-21 19:21:34 +0330

    Create schemas.py

---
+++ app/schemas.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class ExpenseBase(BaseModel):
+    description: str
+    amount: float
+
+
+class ExpenseCreate(ExpenseBase):
+    pass
+
+
+class ExpenseOut(ExpenseBase):
+    id: int
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
+
+
+class ExpenseUpdate(BaseModel):
+    description: Optional[str] = None
+    amount: Optional[float] = None

d712db4d55e5dfa3a0a2a4939f49e30320fc1949
Mahdi <mahdighadiriafzal@gmail.com>
2025-09-21 21:59:38 +0330

    Update models.py

--- app/models.py
+++ app/models.py
@@ -5,6 +5,14 @@ from sqlalchemy import Column, DateTime, Float, Integer, String
 from .database import Base
 
 
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+
+
 class Expense(Base):
     __tablename__ = "expenses"
 

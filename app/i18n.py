868d219df96c41be7af6b96b3c8bfc5db514cd39
Mahdi <mahdighadiriafzal@gmail.com>
2025-09-23 20:26:50 +0330

    Update i18n.py

--- app/i18n.py
+++ app/i18n.py
@@ -1,5 +1,4 @@
 import gettext
-from typing import Optional
 
 from fastapi import Query, Request
 
@@ -7,16 +6,14 @@ SUPPORTED_LANGUAGES = ["en", "fa"]
 DEFAULT_LANGUAGE = "en"
 
 
-def get_language(request: Request, lang: Optional[str] = Query(None)):
+def get_language(request: Request, lang: str | None = Query(None)):
     if lang and lang in SUPPORTED_LANGUAGES:
         return lang
-
     accept_language = request.headers.get("Accept-Language", DEFAULT_LANGUAGE)
     for lang_code in accept_language.split(","):
         lang_code = lang_code.split(";")[0].strip().split("-")[0]
         if lang_code in SUPPORTED_LANGUAGES:
             return lang_code
-
     return DEFAULT_LANGUAGE
 
 

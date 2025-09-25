6ec594b808469d0b4714f2ac69bebe488919f901
Mahdi <mahdighadiriafzal@gmail.com>
2025-09-25 20:40:54 +0330

    Update expenses.py

--- app/crud/expenses.py
+++ app/crud/expenses.py
@@ -4,36 +4,44 @@ from ..models import Expense
 from ..schemas import ExpenseCreate, ExpenseUpdate
 
 
-def create_expense(db: Session, expense: ExpenseCreate):
-    db_expense = Expense(**expense.model_dump())
+def create_expense(db: Session, expense: ExpenseCreate, user_id: int):
+    db_expense = Expense(**expense.model_dump(), user_id=user_id)
     db.add(db_expense)
     db.commit()
     db.refresh(db_expense)
     return db_expense
 
 
-def get_expenses(db: Session):
-    return db.query(Expense).all()
+def get_expenses(db: Session, user_id: int):
+    return db.query(Expense).filter(Expense.user_id == user_id).all()
 
 
-def get_expense(db: Session, expense_id: int):
-    return db.query(Expense).filter(Expense.id == expense_id).first()
+def get_expense(db: Session, expense_id: int, user_id: int):
+    return (
+        db.query(Expense)
+        .filter(Expense.id == expense_id, Expense.user_id == user_id)
+        .first()
+    )
 
 
-def update_expense(db: Session, expense_id: int, expense: ExpenseUpdate):
-    db_expense = get_expense(db, expense_id)
+def update_expense(
+    db: Session, expense_id: int, expense: ExpenseUpdate, user_id: int
+):
+    db_expense = get_expense(db, expense_id, user_id)
     if not db_expense:
         return None
-    update_data = expense.dict(exclude_unset=True)
+
+    update_data = expense.model_dump(exclude_unset=True)
     for key, value in update_data.items():
         setattr(db_expense, key, value)
+
     db.commit()
     db.refresh(db_expense)
     return db_expense
 
 
-def delete_expense(db: Session, expense_id: int):
-    db_expense = get_expense(db, expense_id)
+def delete_expense(db: Session, expense_id: int, user_id: int):
+    db_expense = get_expense(db, expense_id, user_id)
     if db_expense:
         db.delete(db_expense)
         db.commit()
